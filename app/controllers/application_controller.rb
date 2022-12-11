e1dd75b458c2795b6800bcd74d2008e98ea207bb
CreeGunning <creenike@gmail.com>
2022-12-11 19:22:54 +0000

    Search Bar Added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    before_action :set_query
+
+def set_query
+    @query = CustomCharacter.ransack(params[:q])
+end
+
 end
---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,6 @@
+class SearchController < ApplicationController
+  def index
+    @query = CustomCharacter.ransack(params[:q])
+    @custom_characters = @query.result(disctinct: true)
+  end
+end
